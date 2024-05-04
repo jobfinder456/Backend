d9a1d1d3835b516143bc9f4fcc98645047adcfff
Nikhil Chopra <Nikhilchopra788@gmail.com>
2024-05-05 02:59:17 +0530

    created https server

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 const express = require("express");
+const https = require('https');
+const fs = require('fs');
+const cors = require("cors");
+const job = require('./routes/jobs');
+const user = require('./routes/user');
+
+// Create an Express application
 const app = express();
-const cors = require("cors")
-const job = require('./routes/jobs')
-const user = require('./routes/user')
 
-app.use(cors())
-app.use("/api/v1", job)
-app.use("/api/v1", user)
+// Enable CORS
+app.use(cors());
 
-app.listen(8282, () => {
-    console.log("server started in port 8282");
+// Define routes
+app.use("/api/v1", job);
+app.use("/api/v1", user);
+
+// Paths to your SSL certificate and key files
+const privateKeyPath = 'cert/ssl/private.key';
+const certificatePath = 'cert/ssl/certificate.crt';
+
+
+// Read the SSL certificate and key files
+const privateKey = fs.readFileSync(privateKeyPath, 'utf8');
+const certificate = fs.readFileSync(certificatePath, 'utf8');
+
+// Create credentials object
+const credentials = { key: privateKey, cert: certificate };
+
+// Define the port number
+const PORT = 443;
+
+// Create the HTTPS server using Express
+const server = https.createServer(credentials, app);
+
+// Start listening on the specified port
+server.listen(PORT, () => {
+  console.log(`Server running at ${PORT}/`);
 });
