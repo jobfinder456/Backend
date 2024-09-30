7240ee67b8ab11adc76b12b4ff242bc8f0d34d3c
Nikhil Chopra <Nikhilchopra788@gmail.com>
2024-09-30 22:53:22 +0530

    f

--- auth/middleware.js
+++ auth/middleware.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
-  const token = req.cookies?.token;
+  const token = req.cookies.token;
   if (!token) {
     return res.status(403).json({ message: "Access denied. No token provided." });
   }
   
   try {
-    const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
     req.email = decoded.email; 
     next();
   } catch (error) {
